126e42a07ac0670724758f8ced4bd35d6fe88858
Andras Gyomrey <lgandras@google.com>
2021-03-15 18:40:36 +0000

    Top Talkers initial commit

---
+++ tools/vpc-flowlogs-toptalkers/variables.tf
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "vpc_project_id" {
+    type = string
+}
+
+variable "logs_project_id" {
+    type = string
+}
+
+variable "interconnect_ip_ranges_include" {
+    type = string
+    default = ["10.0.0.0/23"]
+}
+
+variable "interconnect_ip_ranges_exclude" {
+    type = string
+    default = ["10.0.0.0/24"]
+}
+
+variable "dataset_name" {
+    type = string
+    default = "vpc_flowlogs_dataset"
+}
+
+variable "location" {
+    type = string
+    description = "GCP location, i.e. (multi-)region, where resources will be created"
+    default = "EU"
+}
